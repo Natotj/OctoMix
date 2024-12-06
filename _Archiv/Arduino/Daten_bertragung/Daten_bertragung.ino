1190d3dbc41e0413a645bb21c6a923aac5554c25
Natotj <natos.naz@gmail.com>
2024-12-06 19:01:27 +0100

    Changes to the valve design and producing 3D printing files

--- _Archiv/Arduino/Daten_bertragung/Daten_bertragung.ino
+++ _Archiv/Arduino/Daten_bertragung/Daten_bertragung.ino
@@ -25,10 +25,7 @@ int a[11]={0, 2, 7, 11, 15, 21, 35, 60, 95, 140, 255};
 //          9 - Ventil rear close
 //         10 - Ventil front close
 //int a[10]={3, 10, 22, 40, 64, 93, 127, 165, 207, 255}; alte werte
-int w=0;
-String save="";
-String save2="";
-int test=0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -37,64 +34,19 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  /*
-  Serial.println("vor"); //vor
-  analogWrite(DIODE_ROT, 7);
+
+delay(2000);
+
+  analogWrite(LED, a[3]);
+  Serial.println("vorwäts");
   delay(1000);
-
-  Serial.println("0"); 
-  analogWrite(DIODE_ROT, 0);
-  delay(500);
-
-  Serial.println("Ventil 1");
-  analogWrite(DIODE_ROT, 80);
+  analogWrite(LED, 0);
+  Serial.println("stop");
   delay(1000);
-
-  Serial.println("0");
-  analogWrite(DIODE_ROT, 0);
-  delay(500);
-
-
-  Serial.println("zurück"); //zurück
-  analogWrite(DIODE_ROT, 25);
+  analogWrite(LED, a[6]);
+  Serial.println("rückwärts");
   delay(1000);
-  
-  Serial.println("Ventil 2");
-  analogWrite(DIODE_ROT, 200);
-  delay(1000);
-
-    Serial.println("0");
-  analogWrite(DIODE_ROT, 0);
-  delay(500);
-*/
-//analogWrite(LED, a[7]);
-delay(500);
-  for(int i=0; i<=10; i++)
-  {
-    //i=random(0,10);
-    analogWrite(LED, a[i]);
-    delay(5000);
-    analogWrite(LED, 0);
-    Serial.print(i);
-    Serial.println();
-    delay(50);
-  } 
-/*  if(Serial.available()>0)
-  {
-    while(Serial.available()>0)
-    {
-      w=Serial.read();
-      w=w-48;
-      save+=w;
-      delay(30);
-    }
-    w=save.toInt();
-    save="";
-    analogWrite(LED, w);
-    Serial.print("LED auf Wert:");
-    Serial.println(w);
-  }
-*/
+  analogWrite(LED, 0);
+  Serial.println("stop");
 
 }
