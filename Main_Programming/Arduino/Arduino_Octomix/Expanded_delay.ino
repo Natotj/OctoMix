2d0c9b60a5871816f40a77e37d20520b0a2776d8
LamaV10 <lama.customs@gmail.com>
2024-12-28 12:49:19 +0100

    Addition of the function Expanded_delay.

---
+++ Main_Programming/Arduino/Arduino_Octomix/Expanded_delay.ino
@@ -0,0 +1,10 @@
+// funktion that takes in your desired delay & keeps otherthings (like the Display_Com()) upright
+void Expanded_delay(int userDelay){
+
+  for (int i = 0; i < userDelay; i += 100){
+    Display_Com();
+    delay(100);
+  }
+  // rest of the user delay
+  delay(userDelay % 100);
+}
