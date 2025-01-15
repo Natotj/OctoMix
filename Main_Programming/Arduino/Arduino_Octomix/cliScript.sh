6e342329415b7128d2e367f569109a29faa408a8
LamaV10 <lama.customs@gmail.com>
2025-01-15 20:08:44 +0100

    cliScript.sh: Accadentally uploaded a not functioning version. This one works.

--- Main_Programming/Arduino/Arduino_Octomix/cliScript.sh
+++ Main_Programming/Arduino/Arduino_Octomix/cliScript.sh
@@ -16,9 +16,12 @@ elif [ $options -eq 3 ]; then
   echo "Enter baudrate: "
   read baudrate
 
-  arduino-cli monitor -p /dev/ttyACM0 --config baudrate=$baudrate
+  arduino-cli monitor -p /dev/ttyACM0 --config baudrate=31250
 
 elif [ $options -eq 4 ]; then
+  echo "Enter baudrate: "
+  read baudrate
+
   arduino-cli compile -p /dev/ttyACM0
   arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno
   arduino-cli monitor -p /dev/ttyACM0 --config baudrate=$baudrate
