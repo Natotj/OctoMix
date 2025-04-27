72bbd56212997e58822b805272a521f662a719f4
LamaV10 <lama.customs@gmail.com>
2025-04-27 20:19:23 +0200

    Deleted cliScript.sh out of the root directory -> only belongs into Main_Programming/Arduino/Arduino_Octomix.

--- Main_Programming/Arduino/Arduino_Octomix/cliScript.sh
+++ Main_Programming/Arduino/Arduino_Octomix/cliScript.sh
@@ -1,7 +1,8 @@
 echo "(1): Compile sketch"
 echo "(2): Upload sketch"
-echo "(3): Monitor sketch"
-echo "(4): Everything"
+echo "(3): Compile & upload"
+echo "(4): Monitor sketch"
+echo "(5): Everything"
 echo ""
 
 read options
@@ -9,12 +10,16 @@ read options
 if [ $options -eq 1 ]; then
   arduino-cli compile -p /dev/ttyACM0
 
-# upload 
+# upload
 elif [ $options -eq 2 ]; then
   arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno
 
+elif [ $options -eq 3 ]; then
+  arduino-cli compile -p /dev/ttyACM0
+  arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno
+
 # monitor sketch or everything (they both need the baudrate setup)
-elif [ $options -eq 3 ] || [ $options -eq 4 ]; then
+elif [ $options -eq 4 ] || [ $options -eq 5 ]; then
   echo "Baudrate: "
   echo "(1): 31250"
   echo "(2): 9600"
@@ -22,10 +27,10 @@ elif [ $options -eq 3 ] || [ $options -eq 4 ]; then
   echo ""
   read baudrateOptions
 
-  # Checks if you want a 31250 or a custom baudrate 
+  # Checks if you want a 31250 or a custom baudrate
   if [ $baudrateOptions -eq 1 ]; then
     baudrate=31250
-  elif [ $baudrateOptions -eq 1 ]; then
+  elif [ $baudrateOptions -eq 2 ]; then
     baudrate=9600
   elif [ $baudrateOptions -eq 3 ]; then
     echo ""
@@ -33,11 +38,11 @@ elif [ $options -eq 3 ] || [ $options -eq 4 ]; then
     read baudrate
   fi
 
-  # Commands (3 monitoring and 4 everything) 
-  if [ $options -eq 3 ]; then
+  # Commands (4 monitoring and 5 everything)
+  if [ $options -eq 4 ]; then
     arduino-cli monitor -p /dev/ttyACM0 --config baudrate=31250
-   
-  elif [ $options -eq 4 ]; then
+
+  elif [ $options -eq 5 ]; then
     arduino-cli compile -p /dev/ttyACM0
     arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno
     arduino-cli monitor -p /dev/ttyACM0 --config baudrate=$baudrate
