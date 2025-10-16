5c5d9bd2eebb678cfb9d21c9262d27641d2599a9
LamaV10 <lama.customs@gmail.com>
2025-10-16 20:31:36 +0200

    Imporved cliScript.sh with default baudrate of 9600.

--- Main_Programming/Arduino/Arduino_Octomix/cliScript.sh
+++ Main_Programming/Arduino/Arduino_Octomix/cliScript.sh
@@ -1,4 +1,4 @@
-board="uno"
+board=uno
 echo "Current board: $board"
 
 echo "(1): Compile sketch"
@@ -25,23 +25,8 @@ elif [ $options -eq 3 ]; then
 
 # monitor sketch or everything (they both need the baudrate setup)
 elif [ $options -eq 4 ] || [ $options -eq 5 ]; then
-  echo "Baudrate: "
-  echo "(1): 31250"
-  echo "(2): 9600"
-  echo "(3): Custom"
-  echo ""
-  read baudrateOptions
-
-  # Checks if you want a 31250 or a custom baudrate
-  if [ $baudrateOptions -eq 1 ]; then
-    baudrate=31250
-  elif [ $baudrateOptions -eq 2 ]; then
-    baudrate=9600
-  elif [ $baudrateOptions -eq 3 ]; then
-    echo ""
-    echo "Enter baudrate: "
-    read baudrate
-  fi
+  read -p "Baudrate [Standard: 9600]: " baudrate
+  baudrate=${baudrate:-9600}
 
   # Commands (4 monitoring and 5 everything)
   if [ $options -eq 4 ]; then
@@ -58,4 +43,5 @@ elif [ $options -eq 6 ]; then
     echo "Enter boardname:"
     read board
     arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:$board
+    sed -i "1s/.*/board=$board/" cliScript.sh
 fi
